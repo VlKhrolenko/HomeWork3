1e53e1a013027da8eb81a1072c55054e2e141ab1
Khrolenko Vladyslav <VLKhrolenko@gmail.com>
2023-08-29 16:25:00 +0300

    hw3

--- js/script2.js
+++ js/script2.js
@@ -3,7 +3,7 @@
 
 const yearForCheck = prompt("Введите год,для проверки на высокосность:")
 
-if (yearForCheck%4 === 0 ){
+if ((yearForCheck % 4 === 0 && yearForCheck % 100 !== 0) || yearForCheck % 400 === 0){
     console.log(`Год ${yearForCheck} является высокосным`)
 } else{
     console.log(`Год ${yearForCheck} не является высокосным`)
